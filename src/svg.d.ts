b11b14ca9e367077c32267e07b23d737cad4d6e8
Nishanth-12345 <nishanth70000@gmail.com>
2024-11-19 23:18:43 +0530

    Clearing remote repository

--- src/svg.d.ts
+++
@@ -1,7 +0,0 @@
-// src/svg.d.ts
-declare module '*.svg' {
-    import React from 'react';
-    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-    const src: string;
-    export default src;
-}
